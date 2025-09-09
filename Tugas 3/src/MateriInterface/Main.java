cf06290de7349a78ae6465d6cb55d14afc8637c4
RiskaAml <amelriskaaa@gmail.com>
2025-09-09 11:08:41 +0700

    Add files via upload

---
+++ Tugas 3/src/MateriInterface/Main.java
@@ -0,0 +1,12 @@
+package MateriInterface;
+
+public class Main {
+     public static void main(String[] args){
+        Pembayaran tokoMajuJaya = new Pembayaran();
+        
+        tokoMajuJaya.pakaiCash();
+        tokoMajuJaya.pakaiQris();
+        tokoMajuJaya.pakaiTransferBank();
+        
+    }
+}
